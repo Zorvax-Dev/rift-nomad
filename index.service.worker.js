5f194e58e5c4ccf04eebb8b83d80c1a9f43e6cca
Zorvax-Dev <julien.rgld@gmail.com>
2026-09-13 07:14:09 +0200

    Safe

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789275669|4238293';
+const CACHE_VERSION = '1789276380|4458122';
 /** @type {string} */
 const CACHE_PREFIX = 'Rift Nomad-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
